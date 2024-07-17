23e7942797104b45b0243412cb5fa84ed2cbb01f
ANISH\anish <anish.saravanan03@gmail.com>
2024-07-17 16:12:45 -0500

    Co-authored-by: janicethomas121 <janicethomas121@users.noreply.github.com>

--- Test_Script.py
+++ Test_Script.py
@@ -1,4 +1,14 @@
 #First Test Script 
 #Testing 2
 
-cars = []
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.kbb.com/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="homeBestCars")
+print(results.prettify())
+
